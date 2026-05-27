1b408d4f552975f1599f9ceaf2636c4540b1e9f8
sandhya-ps <107557924+sandhya-ps@users.noreply.github.com>
2026-05-27 17:50:07 +0800

    Add files via upload

---
+++ day1/FireFox.java
@@ -0,0 +1,17 @@
+package org.testleaf.week1.day1;
+
+public class FireFox {
+    public static void main(String[] args) {
+        float browserVersion = 100.2f;
+        System.out.println(browserVersion);
+        String browserName = "firefox";
+        System.out.println(browserName);
+        boolean isVisible = true;
+        System.out.println(isVisible);
+        int releaseYear = 1998;
+        System.out.println(releaseYear);
+        char browserLogo = 'f';
+        System.out.println(browserLogo);
+
+    }
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,8 @@
+package org.testleaf.week1.day1;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, This is my first javaprogram");
+    }
+
+}
